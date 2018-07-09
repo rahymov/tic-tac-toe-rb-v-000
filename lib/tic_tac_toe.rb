89d11d4a6df98409e1e6c909d2b9e40f551a9492
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-09 15:54:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ end
 def turn(board)
   puts "Enter a valid position(1-9):"
   user_input = gets.strip
-  converted_position = input_to_index(user_input)
+  position = input_to_index(user_input)
   if valid_move?(board, position)
     move(board, position, player)
   else
