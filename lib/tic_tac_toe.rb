2da9e45317d28c968d3865231e2812c90257d16e
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-09 15:56:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,6 +38,7 @@ def turn(board)
   puts "Enter a valid position(1-9):"
   user_input = gets.strip
   position = input_to_index(user_input)
+  
   if valid_move?(board, position)
     move(board, position, player)
     display_board(board)
